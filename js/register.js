51b1d323de959cfef242f9726126d0e6ba2d29cd
Victor <ukpatavictor48@gmail.com>
2026-09-22 10:45:40 +0100

    push

--- js/register.js
+++ js/register.js
@@ -1,7 +1,7 @@
 (function () {
   "use strict";
 
-  var DEPOSIT_AMOUNT = 25000;
+  var DEPOSIT_AMOUNT = 75000; // NGN 75,000
 
   function ready(fn) {
     if (document.readyState !== "loading") fn();
